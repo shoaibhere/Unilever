d1163fa5eb793c8c5fbb0b53d6c981b486197323
shoaibhere <shoaibakhtar321@icloud.com>
2025-01-29 02:43:10 +0500

    Update portfolio.controller.js

--- FinalLabExam/routes/portfolio/portfolio.controller.js
+++ FinalLabExam/routes/portfolio/portfolio.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 let router = express.Router();
 router.get("/",(req,res)=>{
-  return res.redirect("https://usmaniagarden.com");
+  return res.redirect("https://shoaibakhterportfolio.netlify.app/");
 })
 module.exports = router;
